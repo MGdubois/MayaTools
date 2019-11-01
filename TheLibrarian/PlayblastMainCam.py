8502cab0b374143dd0683e427736474c0f649feb
happysir100 <matt.g.dubois@gmail.com>
2019-10-31 20:01:10 -0400

    Playblaster update
    -Works in maya 2018

--- TheLibrarian/PlayblastMainCam.py
+++ TheLibrarian/PlayblastMainCam.py
@@ -1,32 +1,32 @@
 from maya import cmds
 
+
 class playBlaster(object):
 
     def __init__(self):
         self.mainCam = cmds.ls(selection=True, dag=True, long=True)
 
-    #Automatically looks through the selected camera, turns off all elements except polys and then playblasts, afterwards turns all elements back on.
+    # Automatically looks through the selected camera, turns off all elements except polys and then playblasts, afterwards turns all elements back on.
     def selectAndBlast(self):
 
-        #Checks for initial selection
-        if (cmds.ls(selection= True)):
+        # Checks for initial selection
+        if (cmds.ls(selection=True)):
 
             camNode = self.mainCam[1]
             camNodeType = cmds.nodeType(camNode)
 
-            #Checks if selection is a camera
+            # Checks if selection is a camera
             if camNodeType == 'camera':
 
                 cmds.lookThru(self.mainCam)
 
-                currentPanel = cmds.getPanel(withFocus = True)
+                currentPanel = cmds.getPanel(withFocus=True)
 
                 cmds.modelEditor(currentPanel, e=1, allObjects=0, polymeshes=1)
 
-
-                cmds.playblast(format= 'avi', filename= "movies/playblast", forceOverwrite= True,
-                               sequenceTime= 0, clearCache= True, showOrnaments= False, viewer= True, fp= 4,
-                               percent= 100, compression= "none", quality= 100)
+                cmds.playblast(format='avi', filename="movies/playblast", forceOverwrite=True,
+                               clearCache=True, showOrnaments=False, viewer=True,
+                               percent=100, compression="none", quality=100)
 
                 cmds.modelEditor(currentPanel, e=1, allObjects=1)
 
@@ -34,20 +34,30 @@ class playBlaster(object):
 
             else:
                 cmds.error("Select a camera")
-                #print "That ain't no camera"
+                # print "That ain't no camera"
 
         else:
             cmds.error("Select a camera")
-            #print "You need to select a camera"
-
+            # print "You need to select a camera"
 
     def turnTableBlast(self):
-        currentCam = cmds.lookThru(q=True)
-        print currentCam
+        turnCamName = 'TurnCam'
         newCam = cmds.camera()
-        cmds.matchTransform(newCam, cmds.ls(selection=True))
 
+        cmds.rename(newCam[0], turnCamName)
 
+        perspCam = 'persp'
+        
+        #cmds.rename(newCam[0], 'NewCam')
+
+        cmds.matchTransform(turnCamName, perspCam)
+        cmds.lookThru(turnCamName)
+
+        cmds.playblast(format='avi', filename="movies/playblast", forceOverwrite=True,
+                       clearCache=True, showOrnaments=False, viewer=True,
+                       percent=100, compression="none", quality=100)
+        cmds.lookThru(perspCam)
+        cmds.delete(turnCamName)
 
 '''
 currentPanel = cmds.getPanel(withFocus = True)
